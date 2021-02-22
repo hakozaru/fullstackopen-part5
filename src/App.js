bd689b2530a12601390be1419a073c7ca133edaf
hakozaru <t.morii@everyleaf.com>
2021-02-22 09:43:04 +0900

    演習5-2終了

--- src/App.js
+++ src/App.js
@@ -15,6 +15,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('bloglistAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,6 +31,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'bloglistAppUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -34,10 +46,21 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
+
   const blogList = () => {
     return(
       <div>
         <h2>blogs</h2>
+        <div>
+          {user.username} logged in
+          <button onClick={handleLogout}>logout</button>
+        </div>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
