128bdd8ea6f0b7131c5a5355a900a538c702bafc
hakozaru <t.morii@everyleaf.com>
2021-02-24 09:38:51 +0900

    演習5-7終了

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const buttonLabel = () => visible ? 'hide' : 'view'
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={() => setVisible(!visible)}>{buttonLabel()}</button><br />
+      {
+        visible &&
+        <>
+          {blog.url}<br />
+          {blog.likes}<button>like</button><br />
+          {blog.author}<br />
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
